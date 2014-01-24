aebfc972e3852cb97cdeefd098deec73ded6e3b8
Davor Jovanoski <djovanoski@gmail.com>
2014-01-25 00:26:21 +0100

    add start buttton with the pause

---
+++ Assets/Davor/Script/GUIButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIButton : MonoBehaviour
+{
+		public int posX = 15;
+		public int posY = 15;
+		public int width = 150;
+		public int height = 30;
+		public string buttonName = "START";
+		public bool paused = true;
+		// Use this for initialization
+		void Start ()
+		{
+			
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				GUITexture guiText = GetComponent<GUITexture> ();
+				if (paused != guiText.enabled) {
+						paused = guiText.enabled;
+						
+				} 
+		}
+		private void OnGUI ()
+		{
+				if (paused) {
+						GUI.Button (new Rect (posX, posY, width, height), buttonName);
+				}
+		}
+}
