7de899306a3bb1c8f0d0300a7aa59cb243400dc1
Elisabethabc <bronedere@gmail.com>
2014-01-25 13:20:50 +0100

    Collisions + Pill
    Collisions + Pill

---
+++ Assets/Elisabeth/Scripts/Pill_FlyPath.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pill_FlyPath : MonoBehaviour {
+
+
+	private List<Vector3> curve_points;
+	public GameObject target;
+	private bool followed = false;
+	private int point_index = 0;
+	public float velocity = 4.0f;
+	private float threshold = 0.1f;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followed)
+		{
+			Vector3 vec = (curve_points[point_index] - curve_points[point_index - 1]).normalized;
+			this.transform.position = this.transform.position + Time.deltaTime * vec;
+			if((this.transform.position - curve_points[point_index]).magnitude < threshold)
+			{
+				if(point_index < (curve_points.Count - 1))
+				   point_index++;
+
+			}
+		}
+	
+	}
+
+	public void setTarget(GameObject obj, List<Vector3> points)
+	{
+		target = obj;
+		followed = true;
+		curve_points = points;
+		point_index = 1;
+
+		Debug.Log ("Particles");
+		GameObject.Instantiate ( Resources.Load ("Pill_Particles") as GameObject);
+		GameObject particles = GameObject.Find ("Pill_Particles(Clone)");
+		particles.name = "Pill_Particles";
+		particles.transform.position = this.transform.position;
+		particles.transform.parent = this.gameObject.transform;
+
+
+		Vector3 vec = (curve_points[point_index] - this.gameObject.transform.position).normalized;
+		Vector3 target_pos =  this.gameObject.transform.position -  20.0f * vec;
+		particles.transform.LookAt(target_pos);
+
+
+
+
+		//this.gameObject.AddComponent<ParticleSystem>();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject == this.target)
+		{
+			Debug.Log ("Destroy");
+			if((col.gameObject.name == "Player1") || (col.gameObject.name == "Player2"))
+			{
+				GameObject obj = null;
+				Vector3 pos = Vector3.zero;
+				Quaternion rot = Quaternion.identity;
+				if(col.gameObject.name == "Player2")
+				{
+					 obj = GameObject.Find ("Player2");
+					 pos = obj.transform.position;
+					 rot = obj.transform.rotation;
+				}
+				else if(col.gameObject.name == "Player1")
+				{
+					obj = GameObject.Find ("Player1");
+					pos = obj.transform.position;
+					rot = obj.transform.rotation;
+				}
+				
+				float r = Random.value;
+				
+				int s = 0;
+				r = 0.0f;
+				string name = " ";
+				if((r >= 0.0f) && (r < (1.0f/5.0f)))
+				{
+					name = "Rock";
+					s = 0;
+				}
+				if((r >= (1.0f/5.0f)) && (r < (2.0f/5.0f)))
+				{
+					name = "Scissors";
+					s = 1;
+				}
+				if((r >= (2.0f/5.0f)) && (r < (3.0f/5.0f)))
+				{
+					name = "Lizard";
+					s = 2;
+				}
+				if((r >= (3.0f/5.0f)) && (r < (4.0f/5.0f)))
+				{
+					name = "Paper";
+					s = 3;
+				}
+
+				if(r >= (5.0f/6.0f))
+				{
+					name = "Spock";
+					s = 4;
+				}
+				
+				
+				GameObject.Instantiate(Resources.Load (name));
+				GameObject new_obj = GameObject.Find ((name + "(Clone)"));
+				new_obj.transform.position = pos;
+				new_obj.transform.rotation = rot;
+				new_obj.name = obj.name;
+				Destroy (obj);
+
+				if(col.gameObject.name == "Player2")
+				{
+					new_obj.AddComponent ("Player2_Collisions");
+					Player2_Collisions g  = new_obj.GetComponent<Player2_Collisions>();
+					g.status = s;
+					
+					Rigidbody[] rbodies = obj.GetComponents<Rigidbody>();
+					
+					foreach(Rigidbody ri in rbodies)
+					{
+						UnityEditorInternal.ComponentUtility.CopyComponent(ri);
+						UnityEditorInternal.ComponentUtility.PasteComponentAsNew(new_obj);
+					}
+				}
+				else
+				{
+					new_obj.AddComponent ("Player1_Collisions");
+					Player1_Collisions g  = new_obj.GetComponent<Player1_Collisions>();
+					g.status = s;
+					
+					Rigidbody[] rbodies = obj.GetComponents<Rigidbody>();
+					
+					foreach(Rigidbody ri in rbodies)
+					{
+						UnityEditorInternal.ComponentUtility.CopyComponent(ri);
+						UnityEditorInternal.ComponentUtility.PasteComponentAsNew(new_obj);
+					}
+				}
+				Destroy (obj);
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Elisabeth/Scripts/Player2_Collisions.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player2_Collisions : MonoBehaviour {
+
+	public int status = 1;
+	// Use this for initialization
+	private float deltaTime;
+	
+	private float velocity = 10.0f;
+	public bool fly_away = false;
+	public float fly_threshold = 20.0f;
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaTime = Time.deltaTime;
+		
+		Vector3 pos = this.gameObject.transform.position;
+		if(Input.GetKey (KeyCode.LeftArrow))
+			pos.x = pos.x - (deltaTime) * velocity;
+		if(Input.GetKey (KeyCode.RightArrow))
+			pos.x = pos.x + deltaTime * velocity;
+		if(Input.GetKey (KeyCode.UpArrow))
+			pos.z = pos.z + deltaTime * velocity;
+		if(Input.GetKey (KeyCode.DownArrow))
+			pos.z = pos.z - deltaTime * velocity;
+		this.gameObject.transform.position = pos;
+
+		if(fly_away)
+		{
+			Debug.Log ("Fly Away");
+			GameObject obj = GameObject.Find ("Main Camera");
+			Vector3 vec = (obj.transform.position - this.gameObject.transform.position).normalized;
+			this.transform.position = this.transform.position + Time.deltaTime * vec;
+
+			if((obj.transform.position - this.gameObject.transform.position).magnitude < fly_threshold)
+			{
+				this.transform.position = Vector3.zero;
+			}
+			
+		}
+		
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("Collision");
+		if(col.gameObject.name == "Pill")
+		{
+			Pill_FlyPath fly_path = col.gameObject.GetComponent<Pill_FlyPath>();
+			GameObject p1 = GameObject.Find ("Player1");
+			Player1_Collisions c = p1.GetComponent<Player1_Collisions>();
+			if((fly_path.target != this.gameObject)&& (!c.fly_away))
+			{
+				GameObject obj = GameObject.Find ("Player1");
+				List<Vector3>points = new List<Vector3>();
+				points.Add (this.gameObject.transform.position);
+				points.Add (obj.transform.position);
+				fly_path.setTarget (obj, points);
+			}
+
+		}
+
+		if(col.gameObject.name == "Player1")
+		{
+			Debug.Log ("Fly Away");
+			Player1_Collisions g  = col.gameObject.GetComponent<Player1_Collisions>();
+			if(g.status != this.status)
+			{
+				switch(this.status)
+				{
+				case 0: //Rock
+				{
+					if(g.status == 3)//Paper
+						fly_away = true;
+					if(g.status == 4)//Spock
+						fly_away = true;
+				}break;
+				case 1: //Scissors
+				{
+					if(g.status == 4) //Spock
+						fly_away = true;
+					if(g.status == 0) //Rock
+						fly_away = true;
+				}break;
+				case 2: //Lizard
+				{
+					if(g.status == 1) // Scissors
+						fly_away = true;
+					if(g.status == 0) //Rock
+						fly_away = true;
+				} break;
+				case 3: //Paper
+				{
+					if(g.status == 2) //Lizard
+						fly_away = true;
+					if(g.status == 1) //Scissors
+						fly_away = true;
+				} break;
+				case 4: //Spock
+				{
+					if(g.status == 2) // Lizzard
+						fly_away = true;
+					if(g.status == 3) // Paper
+						fly_away = true;
+				}break;
+				}
+				
+				
+				
+			}
+		}
+
+
+	}
+}
