2b9027d1c7e25f6f44c9d7d0c0bedad7df6c4f22
Guilherme <guilhermecsrodrigues@gmail.com>
2014-01-25 19:52:00 +0100

    Camera movement script

---
+++ Assets/Guilherme/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject world;
+	public float distance = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 midpoint = (player2.transform.position + player1.transform.position) / 2;
+
+		Vector3 dir =  midpoint - world.transform.position;
+
+		dir.Normalize();
+
+		transform.position = dir * distance;
+		transform.LookAt(world.transform.position);
+	}
+}
