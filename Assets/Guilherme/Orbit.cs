0782a44db2c7b778bf8ecbeaff2b20399c365cdd
Guilherme <guilhermecsrodrigues@gmail.com>
2014-01-26 13:26:05 +0100

    Added a moon

---
+++ Assets/Guilherme/Orbit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+	float rotationSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,rotationSpeed*Time.deltaTime,0);
+	}
+}
