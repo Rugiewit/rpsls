889c4111e0280403ab200a54551f9a23955c4002
Guilherme <guilhermecsrodrigues@gmail.com>
2014-01-26 21:46:21 +0100

    Fixed the movement to a playable type

--- Assets/Philipp/Scripts/MovePlayer.cs
+++ Assets/Philipp/Scripts/MovePlayer.cs
@@ -4,73 +4,72 @@ using System.Collections;
 public class MovePlayer : MonoBehaviour
 {
 
-		public GameObject worldSphere;
-		public Definitions def;
+	public GameObject worldSphere;
+	public Definitions def;
+	public GameObject camera;
 
-		private Movement movement = new Movement ();
+	private Movement movement = new Movement ();
 
-		// Use this for initialization
-		public void Start ()
-		{
-	
+	// Use this for initialization
+	public void Start ()
+	{
+
+	}
+
+	public void SetMovement (Movement movement)
+	{
+			this.movement = movement;
+	}
+
+
+	// Update is called once per frame
+	public void Update ()
+	{
+		GameObject god = GameObject.Find ("GameDirector");	
+		Pause pause = god.GetComponent<Pause> ();
+		if (pause.pause) {
+				return;
 		}
 
-		public void SetMovement (Movement movement)
-		{
-				this.movement = movement;
+
+		//Debug.Log(this.rigidbody.velocity.sqrMagnitude);
+		if (this.rigidbody.velocity.sqrMagnitude > 0.5f || !(movement.up || movement.down || movement.right || movement.left)) {
+			return;
 		}
 
+		Vector3 gravity = worldSphere.transform.position - this.transform.position;
+		Vector3 guideVector = gravity;
+		guideVector.y += 1;
 
-		// Update is called once per frame
-		public void Update ()
-		{
-				GameObject god = GameObject.Find ("GameDirector");	
-				Pause pause = god.GetComponent<Pause> ();
-				if (pause.pause) {
-						return;
-				}
+		Vector3 myRight = Vector3.Cross (guideVector, gravity);
+		Vector3 myUp = Vector3.Cross (gravity, myRight);
+		myRight.Normalize();
+		myUp.Normalize();
 
+		 
+		gravity.Normalize ();
+		Vector3 playerForce = Vector3.zero;
 
-				//Debug.Log(this.rigidbody.velocity.sqrMagnitude);
-				if (this.rigidbody.velocity.sqrMagnitude > 0.5f) {
-						return;
-				}
-
-				Vector3 gravity = worldSphere.transform.position - this.transform.position; 
-				gravity.Normalize ();
-				Vector3 playerForce = Vector3.zero;
-
-				if (movement.up) {
-						Vector3 red = new Vector3 (0, gravity.y, gravity.z);
-						red.Normalize ();
-						Vector3 dir = Vector3.Cross (Vector3.left, red);
-						playerForce += dir;
-				}
-				if (movement.down) {
-						Vector3 red = new Vector3 (0, gravity.y, gravity.z);
-						red.Normalize ();
-						Vector3 dir = Vector3.Cross (Vector3.right, red);
-						playerForce += dir;
-				}
-				if (movement.left) {
-						Vector3 red = new Vector3 (gravity.x, 0, gravity.z);
-						red.Normalize ();
-						Vector3 dir = Vector3.Cross (Vector3.down, red);
-						playerForce += dir;
-				}
-				if (movement.right) {
-						Vector3 red = new Vector3 (gravity.x, 0, gravity.z);
-						red.Normalize ();
-						Vector3 dir = Vector3.Cross (Vector3.up, red);
-						playerForce += dir;
-				}
-				playerForce.Normalize ();
-
-				playerForce *= def.getPlayerForce ();
-				this.rigidbody.AddForce (playerForce);
-	
-
-				this.transform.LookAt (this.rigidbody.velocity + gravity + this.transform.position, -gravity);
-
+		if (movement.up) {
+			playerForce += myUp;
 		}
+		if (movement.down) {
+			playerForce += -myUp;
+		}
+		if (movement.left) {
+			playerForce += -myRight;
+		}
+		if (movement.right) {
+			playerForce += myRight;
+		}
+
+		playerForce.Normalize ();
+
+		playerForce *= def.getPlayerForce ();
+		this.rigidbody.AddForce (playerForce);
+
+
+		this.transform.LookAt (this.rigidbody.velocity + gravity + this.transform.position, -gravity);
+
+	}
 }
